be8d20ab3c86c8d5fcb8464c676cdac5ac38dfd7
Kaige Ye <yekaige2010@gmail.com>
2017-11-26 21:22:06 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 'y';
+        int xor = 0xa5;
+        int axor = a ^ xor;
+        System.out.printf("%x ^ %x = %x\n", a, xor, axor);
+
+        System.out.println("Hello World!");
+    }
+}
