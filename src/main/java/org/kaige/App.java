f65c2fbe17c4f1894221c6097962c6570d72b3b4
Kaige Ye <ye@kaige.org>
2018-07-04 09:20:16 +0800

    migrate playground to maven

---
+++ src/main/java/org/kaige/App.java
@@ -0,0 +1,13 @@
+package org.kaige;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
